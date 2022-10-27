5650060e574ca07de2b695075f884a0ccc11ce2f
MohamedHamdyRagaaSoliman <114232599+MohamedHamdyRagaaSoliman@users.noreply.github.com>
2022-10-27 14:52:14 +0200

    Add files via upload

---
+++ unit3_embeded_c_header.h
@@ -0,0 +1,60 @@
+/*
+ * unit3_embeded_c_header.h
+ *
+ *  Created on: Oct 27, 2022
+ *      Author: 20101
+ */
+
+#ifndef UNIT3_EMBEDED_C_HEADER_H_
+#define UNIT3_EMBEDED_C_HEADER_H_
+
+
+
+#endif /* UNIT3_EMBEDED_C_HEADER_H_ */
+//#endif /* PLATFORM_TYPES_H_ */
+#ifndef _types_
+#define _types_
+
+typedef signed char					sint8;
+typedef unsigned char				uint8;
+typedef signed short				sint16;
+typedef unsigned short				uint16;
+typedef signed int					sint32;
+typedef unsigned int				uint32;
+typedef signed long long			sint64;
+typedef unsigned long long			uint64;
+
+
+
+typedef volatile signed char					vint8_t;
+typedef volatile unsigned char					vuint8_t;
+typedef volatile signed short					vint16_t;
+typedef volatile unsigned short					vuint16_t;
+typedef volatile signed int						vint32_t;
+typedef volatile unsigned int					vuint32_t;
+typedef volatile signed long long				vint64_t;
+typedef volatile unsigned long long				vuint64_t;
+
+
+#ifndef _types_
+#define _types_
+
+typedef signed char					sint8;
+typedef unsigned char				uint8;
+typedef signed short				sint16;
+typedef unsigned short				uint16;
+typedef signed int					sint32;
+typedef unsigned int				uint32;
+typedef signed long long			sint64;
+typedef unsigned long long			uint64;
+
+
+
+typedef volatile signed char					vint8_t;
+typedef volatile unsigned char					vuint8_t;
+typedef volatile signed short					vint16_t;
+typedef volatile unsigned short					vuint16_t;
+typedef volatile signed int						vint32_t;
+typedef volatile unsigned int					vuint32_t;
+typedef volatile signed long long				vint64_t;
+typedef volatile unsigned long long             vuint64_64;
